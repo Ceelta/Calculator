5740c6baaf9aa01c7acab5f02d837dfb5d529352
Victor <ceelta@outlook.com>
2022-02-13 20:34:27 +0100

    Compilación pro

---
+++ webpack.config.pro.js
@@ -0,0 +1,72 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var RemovePlugin = require('remove-files-webpack-plugin');
+
+var basePath = __dirname;
+
+module.exports = {
+  target: "web",
+  mode: 'production',
+  performance: {
+    hints: false,
+    maxEntrypointSize: 1024000,
+    maxAssetSize: 1024000
+  },
+  context: path.join(basePath, 'src'),
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx']
+  },
+  entry: {
+    app: './app.jsx'
+  },
+  output: {
+    path: path.join(basePath, 'dist/pro'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(woff|ttf|otf|eot|woff2|svg|png|jpg|gif|svg)$/i,
+        use: ["url-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /(\.css$)/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ],
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist/dev'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      hash: true,
+    }),
+    new RemovePlugin({
+      after: {
+        root: './dist/pro',
+        include: [
+          'app.js.LICENSE.txt',
+          'app.js.map'
+        ],
+        trash: true
+      }
+    })
+  ]
+};
